bff293b121c541743ecec714fac84df69fdb8ca6
jyp <sndi9207@naver.com>
2023-12-18 18:29:04 +0900

    메인 진행중

--- frontend/src/Routes/Home/Home.jsx
+++ frontend/src/Routes/Home/Home.jsx
@@ -1,5 +1,6 @@
 import CalendarSwiper from './CalendarSwiper';
 import ImgSwiper from './ImgSwiper';
+import GolfCourseSection from './GolfCourseSection ';
 import './Home.scss'
 
 // import { blue } from '@mui/material/colors'; 
@@ -11,6 +12,7 @@ function HomeList() {
     <div>
       <ImgSwiper/>
       <CalendarSwiper/>
+    <GolfCourseSection/>
     </div>
     <a href="/golf/intro" style={{color:"black"}}>골프소개페이지입니다</a>
     <div className="test1" style={{color:"red"}}>
