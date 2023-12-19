99979f130df56674ef72a90d5d37853b69612519
team1060 <team1060dev@gmail.com>
2023-12-19 10:53:21 +0900

    Feat: mui 기본 설정 변경
    - 폰트 변경
    - reset.css 생성

--- frontend/src/Routes/Home/Home.jsx
+++ frontend/src/Routes/Home/Home.jsx
@@ -2,6 +2,7 @@ import CalendarSwiper from './CalendarSwiper';
 import ImgSwiper from './ImgSwiper';
 import GolfCourseSection from './GolfCourseSection ';
 import './Home.scss'
+import { Button } from '@mui/material';
 
 // import { blue } from '@mui/material/colors'; 
 function HomeList() {
@@ -14,6 +15,7 @@ function HomeList() {
       <CalendarSwiper/>
     <GolfCourseSection/>
     </div>
+    <Button color='primary'></Button>
     <a href="/golf/intro" style={{color:"black"}}>골프소개페이지입니다</a>
     <div className="test1" style={{color:"red"}}>
     <a href="/admin" style={{color:"black"}}>관리자 페이지 들어가기</a>
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
-
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import theme from "./components/style/theme"
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
